2c38d538e33d6d1b34eda04aadcc5d5a60948599
cvmullor <52613767+cvmullor@users.noreply.github.com>
2020-09-17 10:58:36 +0200

    Update coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -15,10 +15,10 @@ do
 	# n reads mapping against each position
 	samtools depth -aa $bamfile.sorted.bam | awk '{ if ($3>0) {print $3} }' > $bamfile.nozero.temp
 	# average coverage
-	awk -v filename=$bamfile.nozero.temp '{ sum += $0; n++ } END { if (n > 0) print (sum / n) * 2" "filename; }' $bamfile.nozero.temp >> mcov_nozero.csv
+	awk -v filename=$bamfile.nozero.temp '{ sum += $0; n++ } END { if (n > 0) print (sum / n)" "filename; }' $bamfile.nozero.temp >> mcov_nozero.csv
 	
 	## All positions
 	samtools depth -aa $bamfile.sorted.bam | awk '{ if ($3>=0) {print $3} }' > $bamfile.all.temp
-	awk -v filename=$bamfile.all.temp '{ sum += $0; n++ } END { if (n > 0) print (sum / n) * 2" "filename; }' $bamfile.all.temp >> mcov_all.csv
+	awk -v filename=$bamfile.all.temp '{ sum += $0; n++ } END { if (n > 0) print (sum / n)" "filename; }' $bamfile.all.temp >> mcov_all.csv
 done
 
